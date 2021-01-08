a33100c85ad01c6a8d8f9ca991b082803864d425
Ryan Winter <ryanwinter@outlook.com>
2021-01-08 09:06:21 -0800

    update tests for gcc 10

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -24,9 +24,15 @@ test('test url', () => {
   expect(gcc.distributionUrl('9-2019-q4', 'linux')).toStrictEqual(
     'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-x86_64-linux.tar.bz2'
   )
+  expect(gcc.distributionUrl('9-2019-q4', 'win32')).toStrictEqual(
+    'https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2019q4/gcc-arm-none-eabi-9-2019-q4-major-win32.zip'
+  )
   expect(gcc.distributionUrl('10-2020-q4', 'linux')).toStrictEqual(
     'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2'
-  )  
+  )
+  expect(gcc.distributionUrl('10-2020-q4', 'win32')).toStrictEqual(
+    'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-win32.zip'
+  )
 })
 
 test('test url response', async () => {
@@ -37,11 +43,16 @@ test('test url response', async () => {
 })
 
 function hasGcc(dir: string): boolean {
-  for (const filename of ['arm-none-eabi-gcc', 'arm-none-eabi-gcc.exe']) {
-    const exe = path.join(dir, 'bin', filename)
-    if (fs.existsSync(exe)) {
-      console.log(`${exe} exists`)
+  for (const exe of ['arm-none-eabi-gcc', 'arm-none-eabi-gcc.exe']) {
+    if (fs.existsSync(path.join(dir, 'bin', exe))) {
+      console.log(`bin exists`)
       return true
+    } else {
+      const filenames = fs.readdirSync(dir)
+      if (fs.existsSync(path.join(dir, filenames[0], 'bin', exe))) {
+        console.log(filenames[0] + `bin exists`)
+        return true
+      }
     }
   }
   return false
@@ -59,8 +70,9 @@ async function tmpInstall(release: string, platform?: string): Promise<void> {
 test(
   'install',
   async () => {
-    await tmpInstall('9-2019-q4', 'darwin')
     await tmpInstall('6-2017-q1', 'win32')
+    await tmpInstall('10-2020-q4', 'win32')
+    await tmpInstall('9-2019-q4', 'linux')
   },
-  10 * 60 * 1000
+  40 * 60 * 1000
 )
